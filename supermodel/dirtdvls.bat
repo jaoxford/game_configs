2dc9a03ab6894ae9a94f479d7b13f6c2a88c62dd
Jacob Oxford <jacoboxford12345@gmail.com>
2024-12-30 19:59:45 +0000

    Supermodel BAT files with FFB toggling.

---
+++ supermodel/dirtdvls.bat
@@ -0,0 +1,3 @@
+call C:\Users\jacob\Documents\GitHub\game_configs\supermodel\write_standard_ffb.bat
+
+Supermodel.exe -outputs=win -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\dirtdvls.zip
---
+++ supermodel/eca.bat
@@ -0,0 +1,3 @@
+call C:\Users\jacob\Documents\GitHub\game_configs\supermodel\write_alternative_ffb.bat
+
+Supermodel.exe -outputs=win -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\eca.zip
