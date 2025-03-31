7f38700e5cf68123f0fe27751a502a22b5081558
Jacob Oxford <jacoboxford12345@gmail.com>
2025-03-31 21:24:03 +0100

    Batch file for launching Densha De Go! Nagoya Tetsudou-hen.

---
+++ duckstation/ddgnagoya.bat
@@ -0,0 +1,2 @@
+E:\Games\Playstation\Emulators\Duckstation\duckstation-qt-x64-ReleaseLTCG.exe -batch -- "E:\Games\Playstation\ROMS\Densha de Go Nagoya Tetsudou-hen (Japan)\Densha de Go! Nagoya Tetsudou-hen (Japan).cue"
+
