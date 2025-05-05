c2abe019252b830cb6c2d49a12f786624b9b4f5d
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-05 19:07:15 +0100

    Batch script for launching Virtua Cop.

---
+++ model2/vcop2.bat
@@ -0,0 +1,5 @@
+:: Need to CD to where emulator is, so that roms can be loaded.
+CD /D E:\Games\Sinden\M2emulator_1.1a\
+
+START E:\Games\Sinden\DemulShooter_v12.2\DemulShooter.exe -target=model2 -rom=vco2
+START E:\Games\Sinden\M2emulator_1.1a\emulator_multicpu.exe vcop2
