7006e40c0d83f296e76d9e7e68a27cdcc2d6568e
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-05 19:06:55 +0100

    Added comment to hotd script.

--- model2/hotd.bat
+++ model2/hotd.bat
@@ -1,3 +1,4 @@
+:: Need to CD to where emulator is, so that roms can be loaded.
 CD /D E:\Games\Sinden\M2emulator_1.1a\
 
 START E:\Games\Sinden\DemulShooter_v12.2\DemulShooter.exe -target=model2 -rom=hotd
