b922d506f7f175cb466ff2cf11cc4f6527bd9c14
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-18 19:21:24 +0100

    Copy confidential mission reshade image when running game.

--- flycast/confidential_mission.bat
+++ flycast/confidential_mission.bat
@@ -1,1 +1,3 @@
+copy "E:\Games\Sinden\flycast-win64-2.4\reshade-shaders\Textures\confmiss.png" "E:\Games\Sinden\flycast-win64-2.4\reshade-shaders\Textures\SweetFX\Layer.png"
+
 "E:\Games\Sinden\flycast-win64-2.4\flycast.exe" "E:\Games\SegaArcade\Naomi\ROMS\confmiss.zip"
