73749e7df622a311258512bb786e91cfb599e31c
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-18 19:21:04 +0100

    Copy hotd2 reshade image when running game.

--- flycast/hotd2.bat
+++ flycast/hotd2.bat
@@ -1,1 +1,3 @@
+copy "E:\Games\Sinden\flycast-win64-2.4\reshade-shaders\Textures\hotd2.png" "E:\Games\Sinden\flycast-win64-2.4\reshade-shaders\Textures\SweetFX\Layer.png"
+
 "E:\Games\Sinden\flycast-win64-2.4\flycast.exe" "E:\Games\SegaArcade\Naomi\ROMS\hotd2.zip"
