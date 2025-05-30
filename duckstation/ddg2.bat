5d4a7dc9cc191d40da5eb884294f237162a94e12
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-31 00:13:13 +0100

    More consistent usage of scripts for duckstation.

--- duckstation/ddg2.bat
+++ duckstation/ddg2.bat
@@ -1,1 +1,1 @@
-E:\Games\Playstation\Emulators\Duckstation\duckstation-qt-x64-ReleaseLTCG.exe -batch -- "E:\Games\Playstation\ROMS\Densha de Go 2 (Japan)\Densha de Go! 2 (Japan).cue"
+start E:\Games\Playstation\Emulators\Duckstation\duckstation-qt-x64-ReleaseLTCG.exe -batch -- "E:\Games\Playstation\ROMS\Densha de Go 2 (Japan)\Densha de Go! 2 (Japan).cue"
--- duckstation/ddgnagoya.bat
+++ duckstation/ddgnagoya.bat
@@ -1,2 +1,1 @@
-E:\Games\Playstation\Emulators\Duckstation\duckstation-qt-x64-ReleaseLTCG.exe -batch -- "E:\Games\Playstation\ROMS\Densha de Go Nagoya Tetsudou-hen (Japan)\Densha de Go! Nagoya Tetsudou-hen (Japan).cue"
-
+start E:\Games\Playstation\Emulators\Duckstation\duckstation-qt-x64-ReleaseLTCG.exe -batch -- "E:\Games\Playstation\ROMS\Densha de Go Nagoya Tetsudou-hen (Japan)\Densha de Go! Nagoya Tetsudou-hen (Japan).cue"
