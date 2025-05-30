488ff644a6b09a0d1ec7088f2adefdfc2f88ce20
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-30 22:04:48 +0100

    No need to copy over Model 2 batch files to where emulator is located.
    All batch scripts can stay in config location, effectively meaning 1 less step is needed to setup Model 2.

--- model2/daytona.bat
+++ model2/daytona.bat
@@ -1,3 +1,6 @@
+:: Need to CD to where emulator is, so that roms can be loaded.
+CD /D E:\Games\SegaArcade\Model2\M2emulator_1.1a\
+
 call C:\Users\jacob\Documents\GitHub\game_configs\model2\write_standard_ffb.bat
 
-Start E:\Games\SegaArcade\Model2\M2emulator_1.1a\emulator_multicpu.exe daytona
+Start emulator_multicpu.exe daytona
