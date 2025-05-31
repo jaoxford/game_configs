f25e442614d14f7df0159d6126043bde2bd3b4c8
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-31 02:06:22 +0100

    Nicer headings for set path scripts.

--- shared/set_rom_paths.bat
+++ shared/set_rom_paths.bat
@@ -1,3 +1,9 @@
+:: ____   ___  __  __ ____
+::|  _ \ / _ \|  \/  / ___|
+::| |_) | | | | |\/| \___ \
+::|  _ <| |_| | |  | |___) |
+::|_| \_\\___/|_|  |_|____/
+::
 @echo off
 
 setx duckstation_roms_location E:\Games\Playstation\ROMS
