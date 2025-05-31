239eaa1d095919ab847f42a586f817560cf5bee4
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-31 01:26:22 +0100

    More standardised way of launching supermodel games.

--- supermodel/daytona2.bat
+++ supermodel/daytona2.bat
@@ -1,4 +1,4 @@
-:: Need to CD to where emulator is, so that roms can be loaded.
-CD /D E:\Games\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
+:: Need to navigate to where emulator is, so that roms can be loaded.
+cd /d E:\Games\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
 
-Supermodel.exe -force-feedback -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\dayto2pe.zip
+start Supermodel.exe -force-feedback -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\dayto2pe.zip
--- supermodel/eca.bat
+++ supermodel/eca.bat
@@ -1,6 +1,6 @@
-:: Need to CD to where emulator is, so that roms can be loaded.
-CD /D E:\Games\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
+:: Need to navigate to where emulator is, so that roms can be loaded.
+cd /d E:\Games\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
 
 call C:\Users\jacob\Documents\GitHub\game_configs\supermodel\write_alternative_ffb.bat
 
-Supermodel.exe -outputs=win -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\eca.zip
+start Supermodel.exe -outputs=win -crtcolors=1 E:\Games\SegaArcade\Model3\ROMs\eca.zip
