716151c78b6da29056c33035f313e986addf4a8b
Jacob Oxford <jacoboxford12345@gmail.com>
2025-05-31 15:59:25 +0100

    Duckstation script banners.

--- duckstation/ddgnagoya.bat
+++ duckstation/ddgnagoya.bat
@@ -1,3 +1,11 @@
+::
+::  _   _
+:: | \ | | __ _  __ _  ___  _   _  __ _
+:: |  \| |/ _` |/ _` |/ _ \| | | |/ _` |
+:: | |\  | (_| | (_| | (_) | |_| | (_| |
+:: |_| \_|\__,_|\__, |\___/ \__, |\__,_|
+::              |___/       |___/
+::
 cd /d "%duckstation_location%"
 
 start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -- "%duckstation_roms_location%\Densha de Go Nagoya Tetsudou-hen (Japan)\Densha de Go! Nagoya Tetsudou-hen (Japan).cue"
