063522f87e847b9e9ee48e9f4c68b23139fe662a
Jacob Oxford <jacoboxford12345@gmail.com>
2025-06-03 22:41:50 +0100

    Commented banners for supermodel games.

--- supermodel/daytona2.bat
+++ supermodel/daytona2.bat
@@ -1,3 +1,11 @@
+::
+::  ____              _                      ____
+:: |  _ \  __ _ _   _| |_ ___  _ __   __ _  |___ \
+:: | | | |/ _` | | | | __/ _ \| '_ \ / _` |   __) |
+:: | |_| | (_| | |_| | || (_) | | | | (_| |  / __/
+:: |____/ \__,_|\__, |\__\___/|_| |_|\__,_| |_____|
+::              |___/
+::
 cd /d "%supermodel_location%"
 
 start "" Supermodel.exe -force-feedback -crtcolors=1 "%supermodel_roms_location%\dayto2pe.zip"
--- supermodel/scudraceplus.bat
+++ supermodel/scudraceplus.bat
@@ -1,3 +1,11 @@
+::
+::  ____                 _   ____
+:: / ___|  ___ _   _  __| | |  _ \ __ _  ___ ___
+:: \___ \ / __| | | |/ _` | | |_) / _` |/ __/ _ \
+::  ___) | (__| |_| | (_| | |  _ < (_| | (_|  __/
+:: |____/ \___|\__,_|\__,_| |_| \_\__,_|\___\___|
+::
+::
 cd /d "%supermodel_location%"
 
 start "" Supermodel.exe -force-feedback -crtcolors=1 "%supermodel_roms_location%\scudplus.zip"
