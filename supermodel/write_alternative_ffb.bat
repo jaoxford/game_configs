78e1b9cd007fd1efa2b4ed3b08d6316a4f55f61c
Jacob Oxford <jacoboxford12345@gmail.com>
2025-06-03 23:06:41 +0100

    Batch scripts for writing supermodel FFB now use relative paths.

--- supermodel/write_alternative_ffb.bat
+++ supermodel/write_alternative_ffb.bat
@@ -1,2 +1,1 @@
-:: Should use relative path and write to the supermodel_location environment variable.
-xcopy /s /y C:\Users\jacob\Documents\GitHub\game_configs\supermodel\FFBPlugin_alternative.ini E:\Games\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64\FFBPlugin.ini
+copy /y "%~dp0\FFBPlugin_alternative.ini" "%supermodel_location%\FFBPlugin.ini"
