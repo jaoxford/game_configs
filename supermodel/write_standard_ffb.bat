ebda9d2d8dd813c1eaff635bf3414f4a51b673ba
Jacob Oxford <jacoboxford12345@gmail.com>
2025-06-03 23:32:56 +0100

    Comments on relative dirs.

--- supermodel/write_standard_ffb.bat
+++ supermodel/write_standard_ffb.bat
@@ -1,1 +1,5 @@
+::
+:: %~dp0 resolves to the full path of the folder in which the batch script resides.
+:: https://stackoverflow.com/a/14942944/4044560
+::
 copy /y "%~dp0\FFBPlugin.ini" "%supermodel_location%\FFBPlugin.ini"
