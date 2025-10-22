bc67d2e81e2389bf380dbcd15557bef44b083084
Jacob Oxford <jacoboxford12345@gmail.com>
2025-10-22 18:19:52 +0100

    Script for launching Densha De Go! 1 on PS1.

---
+++ duckstation/ddg.bat
@@ -0,0 +1,11 @@
+::
+::  ____                 _                 _         ____       _
+:: |  _ \  ___ _ __  ___| |__   __ _    __| | ___   / ___| ___ | |
+:: | | | |/ _ \ '_ \/ __| '_ \ / _` |  / _` |/ _ \ | |  _ / _ \| |
+:: | |_| |  __/ | | \__ \ | | | (_| | | (_| |  __/ | |_| | (_) |_|
+:: |____/ \___|_| |_|___/_| |_|\__,_|  \__,_|\___|  \____|\___/(_)
+::
+::
+cd /d "%duckstation_location%"
+
+start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -- "%duckstation_roms_location%\Densha de Go (Japan)\Densha de Go! (Japan).cue"
