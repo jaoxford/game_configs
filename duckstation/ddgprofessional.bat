02707ab8becfef37a8f0ab74b02f7c525e31f314
Jacob Oxford <jacoboxford12345@gmail.com>
2025-10-28 19:45:20 +0000

    Batch script for launching Densha De Go! Professional.

---
+++ duckstation/ddgprofessional.bat
@@ -0,0 +1,3 @@
+cd /d "%duckstation_location%"
+
+start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -fullscreen -- "%duckstation_roms_location%\Densha de Go Professional Shiyou (Japan)\Densha de Go! Professional Shiyou (Japan).cue"
