c1205bea2c5bbd3733d5cce6e3b7a3b33b7191d3
Jacob Oxford <jacoboxford12345@gmail.com>
2025-11-23 01:55:50 +0000

    Model 2 racers now use model 2 location variable.

--- model2/daytona.bat
+++ model2/daytona.bat
@@ -1,5 +1,4 @@
-:: Need to navigate to where emulator is, so that roms can be loaded.
-cd /d E:\Games\SegaArcade\Model2\M2emulator_1.1a\
+cd /d "%model_2_location%"
 
 :: Should use a relative path here.
 call C:\Users\jacob\Documents\GitHub\game_configs\model2\write_standard_ffb.bat
--- shared/set_emulator_paths.bat
+++ shared/set_emulator_paths.bat
@@ -10,6 +10,7 @@ setx duckstation_location D:\Playstation\Emulators\Duckstation
 setx fbneo_location D:\fbneo
 setx flycast_location D:\SegaArcade\Naomi\Flycast
 setx mame_location D:\mame\mame0265b_64bit
+setx model_2_location D:\SegaArcade\Model2\M2emulator_1.1a\
 setx pcsx2_location D:\PS2\pcsx2-v2.1.156-windows-x64-Qt\
 setx supermodel_location D:\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
 :: ____  _           _
