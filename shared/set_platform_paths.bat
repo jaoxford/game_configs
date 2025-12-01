c942e0083aee9d7d40559a590045a35a69296b85
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-01 23:13:19 +0000

    Backslash to signify the locations are all directories.

--- shared/set_platform_paths.bat
+++ shared/set_platform_paths.bat
@@ -6,13 +6,13 @@
 ::
 @echo off
 
-setx duckstation_location D:\Playstation\Emulators\Duckstation
-setx fbneo_location D:\fbneo
-setx flycast_location D:\SegaArcade\Naomi\Flycast
-setx mame_location D:\mame\mame0265b_64bit
+setx duckstation_location D:\Playstation\Emulators\Duckstation\
+setx fbneo_location D:\fbneo\
+setx flycast_location D:\SegaArcade\Naomi\Flycast\
+setx mame_location D:\mame\mame0265b_64bit\
 setx model_2_location D:\SegaArcade\Model2\M2emulator_1.1a\
 setx pcsx2_location D:\PS2\pcsx2-v2.1.156-windows-x64-Qt\
-setx supermodel_location D:\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64
+setx supermodel_location D:\SegaArcade\Model3\Supermodel_0.3a-git-d043dc0_Win64\
 setx teknoparrot_location D:\Teknoparrot\
 :: ____  _           _
 ::/ ___|(_)_ __   __| | ___ _ __
@@ -20,6 +20,6 @@ setx teknoparrot_location D:\Teknoparrot\
 :: ___) | | | | | (_| |  __/ | | |
 ::|____/|_|_| |_|\__,_|\___|_| |_|
 ::
-setx sinden_flycast_location D:\Sinden\flycast-win64-2.4
+setx sinden_flycast_location D:\Sinden\flycast-win64-2.4\
 
-setx steam_location D:\SteamLibrary\steamapps\common
+setx steam_location D:\SteamLibrary\steamapps\common\
