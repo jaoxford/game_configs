0be96b175638fe8c021814912ea8fc2cbc7481f8
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-06 17:00:43 +0000

    Hotd was missing "" when starting.

--- flycast/hotd2.bat
+++ flycast/hotd2.bat
@@ -15,4 +15,4 @@ cd /d "%sinden_flycast_location%"
 
 copy "%sinden_flycast_location%\reshade-shaders\Textures\hotd2.png" "%sinden_flycast_location%\reshade-shaders\Textures\SweetFX\Layer.png"
 
-start flycast.exe "E:\Games\SegaArcade\Naomi\ROMS\hotd2.zip"
+start "" flycast.exe "E:\Games\SegaArcade\Naomi\ROMS\hotd2.zip"
