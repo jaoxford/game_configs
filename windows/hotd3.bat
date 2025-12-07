ed64b767c820c2e2512242b2755083fe8e59db22
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-07 01:19:21 +0000

    House of the dead 3 now works with 2 players.

--- windows/hotd3.bat
+++ windows/hotd3.bat
@@ -1,1 +1,4 @@
+-target=windows -rom=hod3pc
+
+start "" "%sinden_location%DemulShooter_v12.2\DemulShooter.exe" -target=windows -rom=hod3pc
 start "" "%sinden_location%HOTD3\Hotd3pc_ArcadeMod_v7.0\Hotd3Arcade_Launcher.exe
