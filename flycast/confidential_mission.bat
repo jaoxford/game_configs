d2e8b1c39df1b2cc45a889db75441bac089a802d
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-07 00:12:46 +0000

    Fixing Naomi scripts for Sinden.

--- flycast/confidential_mission.bat
+++ flycast/confidential_mission.bat
@@ -15,4 +15,4 @@ cd /d "%sinden_flycast_location%"
 
 copy "%sinden_flycast_location%\reshade-shaders\Textures\confmiss.png" "%sinden_flycast_location%\reshade-shaders\Textures\SweetFX\Layer.png"
 
-start "" flycast.exe "%flycast_roms_location%\confmiss.zip"
+start "" flycast.exe "%flycast_roms_location%confmiss.zip"
--- flycast/hotd2.bat
+++ flycast/hotd2.bat
@@ -13,6 +13,6 @@
 ::
 cd /d "%sinden_flycast_location%"
 
-copy "%sinden_flycast_location%\reshade-shaders\Textures\hotd2.png" "%sinden_flycast_location%\reshade-shaders\Textures\SweetFX\Layer.png"
+copy "%sinden_flycast_location%reshade-shaders\Textures\hotd2.png" "%sinden_flycast_location%reshade-shaders\Textures\SweetFX\Layer.png"
 
-start "" flycast.exe "E:\Games\SegaArcade\Naomi\ROMS\hotd2.zip"
+start "" flycast.exe "%flycast_roms_location%hotd2.zip"
