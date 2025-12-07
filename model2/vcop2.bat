faf5d70c0545477775aa1616e6e2fc9e38d51a4f
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-07 01:30:26 +0000

    Both Virtua Cop 1 & 2 working 2p in Model 2 emulator.

--- model2/vcop2.bat
+++ model2/vcop2.bat
@@ -1,5 +1,4 @@
-:: Need to navigate to where emulator is, so that roms can be loaded.
-cd /d E:\Games\Sinden\M2emulator_1.1a\
+cd /d "%sinden_model_2_location%"
 
-start E:\Games\Sinden\DemulShooter_v12.2\DemulShooter.exe -target=model2 -rom=vco2
-start E:\Games\Sinden\M2emulator_1.1a\emulator_multicpu.exe vcop2
+start "" "%sinden_location%DemulShooter_v12.2\DemulShooter.exe" -target=model2 -rom=vcop2
+start "" "%sinden_model_2_location%\emulator_multicpu.exe" vcop2
