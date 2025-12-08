df174fb943f0eb2639fff1548aa66a3e4c74d4c3
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-08 19:32:18 +0000

    Adding raw input system for Lost world .

--- supermodel/jurassic_park_lost_world.bat
+++ supermodel/jurassic_park_lost_world.bat
@@ -8,4 +8,4 @@
 ::
 cd /d "%supermodel_location%"
 
-start "" Supermodel.exe "%supermodel_roms_location%\lostwsga.zip" -fullscreen
+start "" Supermodel.exe "%supermodel_roms_location%\lostwsga.zip" -fullscreen -input-system=rawinput
