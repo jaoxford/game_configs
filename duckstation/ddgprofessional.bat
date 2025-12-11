10eccc90e277c871f59f09ec991da5e4595e177a
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-11 23:28:44 +0000

    Commented Banners for all batch scripts.

--- duckstation/ddgprofessional.bat
+++ duckstation/ddgprofessional.bat
@@ -1,3 +1,11 @@
+::
+::  ____                 _                 _         ____       _
+:: |  _ \  ___ _ __  ___| |__   __ _    __| | ___   / ___| ___ | |
+:: | | | |/ _ \ '_ \/ __| '_ \ / _` |  / _` |/ _ \ | |  _ / _ \| |
+:: | |_| |  __/ | | \__ \ | | | (_| | | (_| |  __/ | |_| | (_) |_|
+:: |____/ \___|_| |_|___/_| |_|\__,_|  \__,_|\___|  \____|\___/(_)
+::
+::
 cd /d "%duckstation_location%"
 
 start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -fullscreen -- "%duckstation_roms_location%\Densha de Go Professional Shiyou (Japan)\Densha de Go! Professional Shiyou (Japan).cue"
