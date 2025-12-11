57e502f0c7fdde1e58b4e182750868d4fe046e1f
Jacob Oxford <jacoboxford12345@gmail.com>
2025-12-11 22:54:57 +0000

    Sega rally was using hardcoded path for writing alternative FFB.

--- model2/srally.bat
+++ model2/srally.bat
@@ -1,6 +1,5 @@
 cd /d "%model_2_location%"
 
-:: Should use a relative path here.
-call C:\Users\jacob\Documents\GitHub\game_configs\model2\write_alternative_ffb.bat
+call %~dp0\write_standard_ffb.bat
 
 start "" emulator_multicpu.exe srallyc
