21add52c8201b9ff3ea43038d2b4420baa286275
Jacob Oxford <jacoboxford12345@gmail.com>
2026-01-31 23:29:59 +0000

    Fixed PCSX2 paths.

--- pcsx2/ddg3.bat
+++ pcsx2/ddg3.bat
@@ -8,4 +8,4 @@
 ::
 cd /d "%pcsx2_location%"
 
-start "" pcsx2-qt.exe -fullscreen -- "%pcsx2_roms_location%\Densha de Go 3 - Tsuukin-hen (Japan).chd"
+start "" pcsx2-qt.exe -fullscreen -- "%pcsx2_roms_location%Densha de Go 3 - Tsuukin-hen (Japan).chd"
--- pcsx2/ddgfinal.bat
+++ pcsx2/ddgfinal.bat
@@ -14,4 +14,4 @@
 ::
 cd /d "%pcsx2_location%"
 
-start "" pcsx2-qt.exe -fullscreen -- "%pcsx2_roms_location%\Densha de Go Final (Japan).chd"
+start "" pcsx2-qt.exe -fullscreen -- "%pcsx2_roms_location%Densha de Go Final (Japan).chd"
