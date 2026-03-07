d36ae947e36b31b48ff1b8a35ffe76c61985abe0
Jacob Oxford <jacoboxford12345@gmail.com>
2026-03-07 22:04:35 +0000

    Setup script entry point.

--- windows/setup.bat
+++ windows/setup.bat
@@ -1,4 +1,5 @@
 @echo off
 
-call %~dp0\windows\shared\set_platform_paths.bat
-call %~dp0\windows\shared\set_rom_paths.bat
+call %~dp0\shared\set_platform_paths.bat
+call %~dp0\shared\set_rom_paths.bat
+call %~dp0\sinden\demulshooter\link_init.bat
