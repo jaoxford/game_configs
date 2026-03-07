cfe6fb4208d992c9cbf814acf9577df8810387ec
Jacob Oxford <jacoboxford12345@gmail.com>
2026-03-07 17:49:46 +0000

    Script for killing mame games.

--- windows/sinden/exit_game.bat
+++ windows/sinden/exit_game.bat
@@ -1,6 +1,7 @@
 @echo off
 
 call %~dp0\..\..\flycast\exit_flycast.bat
+call %~dp0\..\..\mame\exit_mame.bat
 call %~dp0\..\..\model2\exit_model_2.bat
 call %~dp0\..\..\supermodel\exit_supermodel.bat
 call %~dp0\..\..\teknoparrot\exit_teknoparrot.bat
