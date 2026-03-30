277b3f87e2444532cc4b37ee074f777e1f545806
Jacob Oxford <jacoboxford12345@gmail.com>
2026-03-30 23:57:46 +0100

    Double slash found in duckstation paths.

--- windows/duckstation/ddgprofessional.bat
+++ windows/duckstation/ddgprofessional.bat
@@ -8,4 +8,4 @@
 ::
 cd /d "%duckstation_location%"
 
-start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -fullscreen -- "%duckstation_roms_location%\Densha de Go Professional Shiyou (Japan)\Densha de Go! Professional Shiyou (Japan).cue"
+start "" duckstation-qt-x64-ReleaseLTCG.exe -batch -fullscreen -- "%duckstation_roms_location%Densha de Go Professional Shiyou (Japan)\Densha de Go! Professional Shiyou (Japan).cue"
