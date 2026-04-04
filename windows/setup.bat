b42ab2ff710db7732044834c4f4a41b4f65b93cd
Jacob Oxford <jacoboxford12345@gmail.com>
2026-04-04 11:37:27 +0100

    Notes for Keychron / controller issues.

--- windows/setup.bat
+++ windows/setup.bat
@@ -11,3 +11,8 @@
 call %~dp0\shared\set_platform_paths.bat
 call %~dp0\shared\set_rom_paths.bat
 call %~dp0\sinden\demulshooter\link_init.bat
+
+:: If having issues with anticheat or other games crashing, double check that keychron is not joy.cpl
+:: If there, go to device manager
+:: Click view -> devices by container -> Keychron Link -> HID Game Compliant Controller
+:: Right click, and disable
