1cd3942fc39b0c9eedc487b447bbb3e9558bebbe
Jacob Oxford <jacoboxford12345@gmail.com>
2026-04-04 14:06:00 +0100

    Linking supermodel cfg properly!

--- windows/supermodel/Config/link_ini.bat
+++ windows/supermodel/Config/link_ini.bat
@@ -1,4 +1,4 @@
 @echo off
 set "CURRENT_DIR=%~dp0"
 if exist "%supermodel_location%\Config\Supermodel.ini" del "%supermodel_location%\Config\Supermodel.ini"
-mklink "%supermodel_location%\Config\Supermodel.ini" "%CURRENT_DIR%Config\Supermodel.ini"
+mklink "%supermodel_location%\Config\Supermodel.ini" "%CURRENT_DIR%Supermodel.ini"
